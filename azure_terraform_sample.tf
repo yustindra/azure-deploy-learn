018c8d9ca33ca293aad09b3814b27e02b6c7a3cf
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-07-29 10:48:17 +0900

    Update azure_terraform_sample.tf

--- azure_terraform_sample.tf
+++ azure_terraform_sample.tf
@@ -1,3 +1,21 @@
+provider "azurerm" {
+  # `version` is optional, but recommended
+  version = "=2.20.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East Asia"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
 resource "random_uuid" "uuid" {}
 
 resource "azurerm_resource_group" "rg" {
