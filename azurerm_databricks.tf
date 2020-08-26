accbe838766558bde3e1885f1ca7ffcb6641d836
A. F. Yustindra <ystndr@gmail.com>
2020-08-26 17:44:32 +0900

    Create azurerm_databricks.tf

---
+++ azurerm_databricks.tf
@@ -0,0 +1,15 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West US"
+}
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "databricks-test"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "standard"
+
+  tags = {
+    Environment = "Production"
+  }
+}
