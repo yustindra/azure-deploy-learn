7a3c16c34b39b731b841d238f747e21b412b9bbd
A. F. Yustindra <ystndr@gmail.com>
2020-08-27 16:55:07 +0900

    Update azure_terraform_sample.tf

--- azure_terraform_sample.tf
+++ azure_terraform_sample.tf
@@ -1,24 +1,20 @@
+# Configure the Azure Provider
 provider "azurerm" {
-  # `version` is optional, but recommended
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.20.0"
   features {}
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "East Asia"
+  location = "West Europe"
 }
 
+# Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
-}
-
-resource "random_uuid" "uuid" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-hello-tf-${random_uuid.uuid.result}"
-  location = var.location
 }
